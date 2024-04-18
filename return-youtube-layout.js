a0e0efb7941e128623aa7605601b376198c4810b
Rztprog <45171753+rztprog@users.noreply.github.com>
2024-04-18 10:30:06 +0200

    Update return-youtube-layout.js
    Rework Garbhj script (https://github.com/garbhj/revert-youtube-layout-UI-change)

--- return-youtube-layout.js
+++ return-youtube-layout.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Return Youtube Layout
 // @namespace    http://github.com/rztprog
-// @version      0.3
+// @version      0.4
 // @description  Disable the experimental Youtube 'Porn Layout' features for a more comfortable viewing experience.
 // @author       Rztprog
 // @match        https://www.youtube.com/*
@@ -21,38 +21,27 @@
     let observersActive = false;
     let observers = [];
     const experimentFlags = yt.config_.EXPERIMENT_FLAGS;
-    /*
-    const targetedElements = [
-        "thumbnail-container",
-        "thumbnail",
-        "label-container",
-        "ytp-inline-preview-scrim",
-        "metadata",
-        "metadata-line",
-        "yt-core-image",
-        "video-title",
-        "ytd-video-meta-block",
-        "ytd-video-renderer",
-        "ytd-rich-grid-media",
-        "ytd-rich-item-rendere",
-        "ytd-video-preview",
-        "ytd-moving-thumbnail-renderer",
-        "ytd-miniplayer",
-        "ytp-size-button",
-        "ytp-miniplayer-expand-watch-page-button",
-        "ytp-miniplayer-scrim",
-        "ytp-play-button",
-    ];
-    */
 
     // ExperimentFlags Variables
     experimentFlags.kevlar_watch_grid = false;
     experimentFlags.small_avatars_for_comments = false;
     experimentFlags.small_avatars_for_comments_ep = false;
+    experimentFlags.kevlar_watch_comments_panel_button = false;
+    experimentFlags.kevlar_watch_comments_ep_disable_theater = true;
+    experimentFlags.kevlar_watch_grid_hide_chips = false;
+    experimentFlags.kevlar_watch_grid_reduced_top_margin_rich_grid = false;
+    experimentFlags.optimal_reading_width_comments_ep = false;
+    experimentFlags.swatcheroo_direct_use_rich_grid = false;
+    experimentFlags.web_watch_compact_comments = false;
+    experimentFlags.web_watch_compact_comments_header = false;
+    experimentFlags.swatcheroo_rich_grid_delay = 0;
+    experimentFlags.wn_grid_max_item_widt = 0;
+    experimentFlags.wn_grid_min_item_width = 0;
 
     // EventListeners
     document.addEventListener('click', (event) => {
         resizeYoutubeWatch();
+        modifySidebarVideos();
     }, true);
 
     // Functions
@@ -63,8 +52,6 @@
             counter++;
             if (window.location.href.includes('youtube.com/watch')) {
                 clearInterval(timer2);
-                console.log("RYL Loaded");
-                // modifyPageElements();
                 const primary = document.querySelector('#columns #primary');
                 const moviePlayer = document.querySelector('#movie_player');
                 const mainVideo = document.querySelector('.html5-main-video');
@@ -73,7 +60,6 @@
 
                 setElementSize(primary, 'max-width', 'fit-content', false);
                 setElementSize(playerContainerOuter, 'max-width', 100, '%');
-                waitForElement('#items .ytd-watch-next-secondary-results-renderer #contents', 'padding-top', 0, 'px', false, false);
 
                 if (!observersActive) {
                     observers.push(observerAndSetSize(primary, 'max-width', 'fit-content', false, false, false));
@@ -119,7 +105,6 @@
 
             if (element !== null) {
                 clearInterval(timer2);
-                // const newValue = elementToChangeStyle == "width" ? element.offsetWidth : element.offsetHeight;
                 setElementSize(element, style, value, unit);
 
                 if (!observersActive) {
@@ -132,159 +117,118 @@
     }
 
     function setElementSize(element, style, value, unit) {
-        if (style == 'max-width') {
-            element.style.maxWidth = `${value}${unit ? unit : ""}`;
-        }
+        const newStyle = `${value}${unit ? unit : ""}`;
 
-        if (style == "width") {
-            element.style.width = `${value}${unit ? unit : ""}`;
-        }
-
-        if (style == "height") {
-            element.style.height = `${value}${unit ? unit : ""}`;
-        }
-
-        if (style == "left") {
-            element.style.left = `${value}${unit ? unit : ""}`;
-        }
-
-        if (style == "padding-top") {
-            element.style.paddingTop = `${value}${unit ? unit : ""}`;
-        }
-    }
-
-    /*
-    function checkIfTargetedElements(element) {
-        for (const target of targetedElements) {
-            if (element.id === target || element.classList.contains(target)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-    */
-
-    /*
-    function removeElementByXPath(xpath) {
-        var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        if (element) {
-            element.parentNode.removeChild(element);
+        switch (style) {
+            case 'max-width':
+                element.style.maxWidth = newStyle;
+                break;
+            case 'width':
+                element.style.width = newStyle;
+                break;
+            case 'height':
+                element.style.height = newStyle;
+                break;
+            case 'left':
+                element.style.left = newStyle;
+                break;
+            case 'padding-top':
+                element.style.paddingTop = newStyle;
+                break;
+            default:
+                // Handle unsupported style properties
+                console.error(`Unsupported style property: ${style}`);
+                break;
         }
     }
 
-    function moveElementById(sourceId, targetId) {
-        var sourceElement = document.getElementById(sourceId);
-        var targetElement = document.getElementById(targetId);
-        if (sourceElement && targetElement) {
-            targetElement.appendChild(sourceElement);
+    // Help with Garbhj script (https://github.com/garbhj/revert-youtube-layout-UI-change)
+    function modifySidebarVideos() {
+        if (window.location.href.includes('youtube.com/watch')) {
+            // Format thumbnails
+            const thumbnailDivs = document.querySelectorAll('div#thumbnail');
+            thumbnailDivs.forEach(function(div) {
+                div.removeAttribute("class"); // Remove existing classes
+
+                // Apply combined styles (168 pixels width, border radius doesn't work)
+                div.style.cssText = `width: 168px; position: absolute; border-radius: 2px;`;
+            });
+
+            // Modify details div spacing: Optimal min spacing was 99px for me - adjust if neccesary
+            const detailsDivs = document.querySelectorAll('div#details');
+            // I just put min-height because one line titles messed up spacing for some reason
+            detailsDivs.forEach(function(div) {
+                div.style.cssText = "padding-left:176px; margin-top:-10px; min-height: 99px;";
+            });
+
+            // Remove margins from contentsDivs (containing both thumbnail and details) to improve spacing
+            const contentsDivs = document.querySelectorAll('div#contents.ytd-rich-grid-row');
+            contentsDivs.forEach(function(div) {
+                div.style.cssText = "margin: 0px;";
+            });
+
+            // Sepecify rich-item-renderer div spacing (includes playlist and ad items as well) because it's quite big by default
+            const richItemDivs = document.querySelectorAll("div#contents.ytd-rich-grid-row > ytd-rich-item-renderer");
+            richItemDivs.forEach(function(div) {
+                div.style.cssText = "margin-left: 0px; margin-right 0px; margin-bottom: 13px;";
+            });
+
+            // Remove channel avatar profile pics
+            const avatarLinks = document.querySelectorAll("a#avatar-link");
+            avatarLinks.forEach(function(link) {
+                link.remove();
+            });
+
+            // Reformat text size and spacing (doesn't quite work yet)
+            const metadataLineDivs = document.querySelectorAll("div#metadata-line");
+            metadataLineDivs.forEach(function(div) {
+                div.style.cssText = "font-size: 1.2rem; line-height: 1.8rem;";
+            });
+
+            // Removes weird gap at the top of secondary recommendations
+            const secondaryDivs = document.querySelectorAll("div#secondary");
+            secondaryDivs.forEach(function(div) {
+                div.style.cssText = "padding-top: 0px;";
+            });
+        } else {
+            // Revert modifications when not on a YouTube watch page
+            const thumbnailDivs = document.querySelectorAll('div#thumbnail');
+            thumbnailDivs.forEach(function(div) {
+                div.style.cssText = '';
+            });
+
+            const detailsDivs = document.querySelectorAll('div#details');
+            detailsDivs.forEach(function(div) {
+                div.style.cssText = '';
+            });
+
+            const contentsDivs = document.querySelectorAll('div#contents.ytd-rich-grid-row');
+            contentsDivs.forEach(function(div) {
+                div.style.cssText = '';
+            });
+
+            const richItemDivs = document.querySelectorAll("div#contents.ytd-rich-grid-row > ytd-rich-item-renderer");
+            richItemDivs.forEach(function(div) {
+                div.style.cssText = '';
+            });
+
+            const avatarLinks = document.querySelectorAll("a#avatar-link");
+            avatarLinks.forEach(function(link) {
+                link.style.cssText = '';
+            });
+
+            const metadataLineDivs = document.querySelectorAll("div#metadata-line");
+            metadataLineDivs.forEach(function(div) {
+                div.style.cssText = '';
+            });
+
+            const secondaryDivs = document.querySelectorAll("div#secondary");
+            secondaryDivs.forEach(function(div) {
+                div.style.cssText = '';
+            });
         }
     }
 
-    function removeAvatarLinks() {
-        var avatarLinks = document.querySelectorAll('a#avatar-link');
-        avatarLinks.forEach(function(link) {
-            link.parentNode.removeChild(link);
-        });
-    }
-
-    function modifyThumbnailDivs() {
-        var thumbnailDivs = document.querySelectorAll('div#thumbnail');
-        thumbnailDivs.forEach(function(div) {
-            div.removeAttribute("class");
-            div.setAttribute("style", "width:167px;position:absolute;");
-        });
-    }
-
-    function modifyDetailsDivs() {
-        var detailsDivs = document.querySelectorAll('div#details');
-        detailsDivs.forEach(function(div) {
-            div.setAttribute("style", "padding-left:173px;margin-top:-20px;");
-        });
-    }
-
-    function modifyContentDivs() {
-        var contentDivs = document.querySelectorAll('div#content');
-        contentDivs.forEach(function(div) {
-
-        });
-    }
-
-    function modifyMarginTopByXPath(xpath, marginTopValue) {
-        var element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        if (element) {
-            element.setAttribute("style", "margin-top: " + marginTopValue);
-        }
-    }
-
-    function removeParagraphsWithNew() {
-        var paragraphs = document.querySelectorAll('p');
-        paragraphs.forEach(function(paragraph) {
-            if (paragraph.textContent.trim() === 'New') {
-                paragraph.parentNode.removeChild(paragraph);
-            }
-        });
-    }
-
-    function clickExpandButton() {
-        var expandButton = document.evaluate("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-grid/div[5]/div[2]/div/ytd-playlist-panel-renderer/div/div[1]/div/div[1]/yt-icon-button/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        if (expandButton && expandButton.getAttribute("aria-label") === "Expand") {
-            expandButton.click();
-        }
-    }
-
-    function modifyPageElements() {
-        removeElementByXPath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-grid/div[5]/div[1]/div/div[2]/div/ytd-watch-next-secondary-results-renderer/div[2]/ytd-rich-grid-renderer/div[1]");
-        moveElementById("secondary-inner", "bottom-grid");
-        removeAvatarLinks();
-        modifyThumbnailDivs();
-        modifyDetailsDivs();
-        modifyContentDivs();
-        removeParagraphsWithNew();
-        modifyMarginTopByXPath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-grid/div[5]/div[2]/ytd-watch-next-secondary-results-renderer/div[2]/ytd-rich-grid-renderer/div[5]", "-20px");
-
-        /* Video Description Container
-    var bottomRow = document.getElementById("bottom-row");
-    if (bottomRow) {
-        bottomRow.setAttribute("style", "margin-top: -10px; padding-bottom: 5px;");
-        var descriptionInner = document.getElementById("description-inner");
-        if (descriptionInner) {
-            descriptionInner.insertBefore(bottomRow, descriptionInner.firstChild);
-        }
-    }
-    */
-
-        /* Description
-    var aboveTheFold = document.getElementById("above-the-fold");
-    if (aboveTheFold) {
-        aboveTheFold.setAttribute("style", "padding-top: 10px;");
-    }
-    */
-
-        /* Secondary */
-    /*
-        const secondaryInner = document.getElementById("secondary-inner");
-        if (secondaryInner) {
-            secondaryInner.removeAttribute("class");
-        }
-
-
-        const sourceElement = document.evaluate("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-grid/div[5]/div[1]/div/div[2]/div/ytd-watch-next-secondary-results-renderer", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        const targetElement = document.evaluate("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-grid/div[5]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        if (sourceElement && targetElement) {
-            targetElement.appendChild(sourceElement);
-        }
-
-        let urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.has('list') && urlParams.get('list') === 'WL') {
-            removeElementByXPath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-grid/div[5]/div[2]/ytd-watch-next-secondary-results-renderer/div[2]/ytd-rich-grid-renderer/div[5]");
-            clickExpandButton();
-        }
-
-        const channelNameTags = document.querySelectorAll('ytd-channel-name#channel-name');
-        channelNameTags.forEach(function(tag) {
-            tag.style.zIndex = "1";
-        });
-    }
-*/
+    const observer = new MutationObserver(modifySidebarVideos);
+    observers.push(observer.observe(document.body, { childList: true, subtree: true }));
 })();
